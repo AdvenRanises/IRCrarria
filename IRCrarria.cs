914d8f6fc12d8309ca732d7d516034abe8a0c888
AdvenRanises <110085960+AdvenRanises@users.noreply.github.com>
2026-06-20 16:52:42 +0800

    Update IRCrarria.cs

--- IRCrarria.cs
+++ IRCrarria.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using Microsoft.Xna.Framework;
 using Terraria;
 using TerrariaApi.Server;
